51c6f15f62c398ae43bd4acc1f25f34b634914f8
pacosonTang <pacoson@163.com>
2015-10-07 08:10:32 +0800

    creating file in orange_s fs

---
+++ fs_create_file_p366/fs/misc.c
@@ -0,0 +1,104 @@
+/*************************************************************************//**
+ *****************************************************************************
+ * @file   misc.c
+ * @brief  
+ * @author Forrest Y. Yu
+ * @date   2008
+ *****************************************************************************
+ *****************************************************************************/
+
+/* Orange'S FS */
+
+#include "type.h"
+#include "stdio.h"
+#include "const.h"
+#include "protect.h"
+#include "string.h"
+#include "fs.h"
+#include "proc.h"
+#include "tty.h"
+#include "console.h"
+#include "global.h"
+#include "keyboard.h"
+#include "proto.h"
+#include "hd.h"
+#include "fs.h"
+
+/*****************************************************************************
+ *                                search_file
+ *****************************************************************************/
+/**
+ * Search the file and return the inode_nr.
+ *
+ * @param[in] path The full path of the file to search.
+ * @return         Ptr to the i-node of the file if successful, otherwise zero.
+ * 
+ * @see open()
+ * @see do_open()
+ *****************************************************************************/
+PUBLIC int search_file(char * path)
+{
+	int i, j;
+
+	char filename[MAX_PATH];
+	memset(filename, 0, MAX_FILENAME_LEN);
+	struct inode * dir_inode;
+	if (strip_path(filename, path, &dir_inode) != 0)
+		return 0;
+
+	if (filename[0] == 0)	/* path: "/" */
+		return dir_inode->i_num;
+
+	/**
+	 * Search the dir for the file.
+	 */
+	int dir_blk0_nr = dir_inode->i_start_sect; // 根目录数据的第一个扇区；
+	int nr_dir_blks = (dir_inode->i_size + SECTOR_SIZE - 1) / SECTOR_SIZE;//根目录所占扇区数目；
+	int nr_dir_entries = // 根目录项的个数
+	  dir_inode->i_size / DIR_ENTRY_SIZE; /**
+					       * including unused slots
+					       * (the file has been deleted
+					       * but the slot is still there)
+					       */
+	int m = 0;
+	struct dir_entry * pde;
+	for (i = 0; i < nr_dir_blks; i++) { // 对每个扇区进行遍历；
+		RD_SECT(dir_inode->i_dev, dir_blk0_nr + i);
+		pde = (struct dir_entry *)fsbuf;
+		for (j = 0; j < SECTOR_SIZE / DIR_ENTRY_SIZE; j++,pde++) { //对某个扇区中的每一个目录项进行遍历；
+			if (memcmp(filename, pde->name, MAX_FILENAME_LEN) == 0)
+				return pde->inode_nr;
+			if (++m > nr_dir_entries)
+				break;
+		}
+		if (m > nr_dir_entries) /* all entries have been iterated */
+			break;
+	}
+
+	/* file not found */
+	return 0;
+}
+
+PUBLIC int strip_path(char * filename, const char * pathname,
+		      struct inode** ppinode)
+{
+	const char * s = pathname;
+	char * t = filename;
+	if (s == 0)
+		return -1;
+	if (*s == '/')
+		s++;
+
+	while (*s) {		/* check each character */
+		if (*s == '/')
+			return -1;
+		*t++ = *s++;
+		/* if filename is too long, just truncate it */
+		if (t - filename >= MAX_FILENAME_LEN)
+			break;
+	}
+	*t = 0;
+	*ppinode = root_inode;
+	return 0;
+}
+
